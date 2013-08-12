a9613e35b7eaaf68a4a3bdbe62fb7d6e4ccc7812
carlo morgantini <cmorgantini@zynga.com>
2013-08-12 15:09:28 -0700

    samples cleaned up

--- samples/TestCpp/Classes/LabelTest/LabelTest.h
+++ samples/TestCpp/Classes/LabelTest/LabelTest.h
@@ -303,26 +303,6 @@ private:
     LabelBMFont *label1;
 };
 
-class NewLabelTTFTestLongLine : public AtlasDemo
-{
-public:
-
-    NewLabelTTFTestLongLine();
-
-    virtual std::string title();
-    virtual std::string subtitle();
-};
-
-class NewLabelTTFColorTest : public AtlasDemo
-{
-public:
-
-    NewLabelTTFColorTest();
-
-    virtual std::string title();
-    virtual std::string subtitle();
-};
-
 class NewLabelTTFUnicode : public AtlasDemo
 {
 public:
@@ -332,38 +312,6 @@ public:
     virtual std::string title();
     virtual std::string subtitle();
 };
-
-class NewLabelTTFAlignment : public AtlasDemo
-{
-public:
-    
-    NewLabelTTFAlignment();
-    virtual std::string title();
-    virtual std::string subtitle();
-    
-private:
-    
-    void setAlignmentLeft(Object* sender);
-    void setAlignmentCenter(Object* sender);
-    void setAlignmentRight(Object* sender);
-    void updateAlignment();
-    
-    Label          * _label;
-    TextHAlignment   _horizAlign;
-    
-};
-
-
-class NewLabelTTFFontsTest : public AtlasDemo
-{
-public:
-
-    NewLabelTTFFontsTest();
-
-    virtual std::string title();
-    virtual std::string subtitle();
-};
-
 
 class NewLabelBMFontTest : public AtlasDemo
 {
