cocos2d-cpp/cocos2d-cpp

New Label stuff merged and Label::Alignment replaced with TextVAlignment and TextHAlignment

cocos2dx/text_input_node/CCTextFieldTTF.h
@@ -103,11 +103,11 @@ public:
     //char * description();
 
     /** creates a TextFieldTTF from a fontname, alignment, dimension and font size */
-    static TextFieldTTF * textFieldWithPlaceHolder(const char *placeholder, const Size& dimensions, Label::HAlignment alignment, const char *fontName, float fontSize);
+    static TextFieldTTF * textFieldWithPlaceHolder(const char *placeholder, const Size& dimensions, TextHAlignment alignment, const char *fontName, float fontSize);
     /** creates a LabelTTF from a fontname and font size */
     static TextFieldTTF * textFieldWithPlaceHolder(const char *placeholder, const char *fontName, float fontSize);
     /** initializes the TextFieldTTF with a font name, alignment, dimension and font size */
-    bool initWithPlaceHolder(const char *placeholder, const Size& dimensions, Label::HAlignment alignment, const char *fontName, float fontSize);
+    bool initWithPlaceHolder(const char *placeholder, const Size& dimensions, TextHAlignment alignment, const char *fontName, float fontSize);
     /** initializes the TextFieldTTF with a font name and font size */
     bool initWithPlaceHolder(const char *placeholder, const char *fontName, float fontSize);
 

extensions/CCBReader/CCLabelTTFLoader.cpp
@@ -64,9 +64,9 @@ void LabelTTFLoader::onHandlePropTypeFloatScale(Node * pNode, Node * pParent, co
 
 void LabelTTFLoader::onHandlePropTypeIntegerLabeled(Node * pNode, Node * pParent, const char * pPropertyName, int pIntegerLabeled, CCBReader * pCCBReader) {
     if(strcmp(pPropertyName, PROPERTY_HORIZONTALALIGNMENT) == 0) {
-        ((LabelTTF *)pNode)->setHorizontalAlignment(Label::HAlignment(pIntegerLabeled));
+        ((LabelTTF *)pNode)->setHorizontalAlignment(TextHAlignment(pIntegerLabeled));
     } else if(strcmp(pPropertyName, PROPERTY_VERTICALALIGNMENT) == 0) {
-        ((LabelTTF *)pNode)->setVerticalAlignment(Label::VAlignment(pIntegerLabeled));
+        ((LabelTTF *)pNode)->setVerticalAlignment(TextVAlignment(pIntegerLabeled));
     } else {
         NodeLoader::onHandlePropTypeFloatScale(pNode, pParent, pPropertyName, pIntegerLabeled, pCCBReader);
     }

mpereyra/cocos2d-x

cocos2dx/text_input_node/CCTextFieldTTF.h
@@ -103,11 +103,11 @@ public:
     //char * description();
 
     /** creates a CCTextFieldTTF from a fontname, alignment, dimension and font size */
-    static CCTextFieldTTF * textFieldWithPlaceHolder(const char *placeholder, const CCSize& dimensions, CCTextAlignment alignment, const char *fontName, float fontSize);
+    static CCTextFieldTTF * textFieldWithPlaceHolder(const char *placeholder, const CCSize& dimensions, TextHAlignment alignment, const char *fontName, float fontSize);
     /** creates a CCLabelTTF from a fontname and font size */
     static CCTextFieldTTF * textFieldWithPlaceHolder(const char *placeholder, const char *fontName, float fontSize);
     /** initializes the CCTextFieldTTF with a font name, alignment, dimension and font size */
-    bool initWithPlaceHolder(const char *placeholder, const CCSize& dimensions, CCTextAlignment alignment, const char *fontName, float fontSize);
+    bool initWithPlaceHolder(const char *placeholder, const CCSize& dimensions, TextHAlignment alignment, const char *fontName, float fontSize);
     /** initializes the CCTextFieldTTF with a font name and font size */
     bool initWithPlaceHolder(const char *placeholder, const char *fontName, float fontSize);
 

extensions/CCBReader/CCLabelTTFLoader.cpp
@@ -64,9 +64,9 @@ void CCLabelTTFLoader::onHandlePropTypeFloatScale(CCNode * pNode, CCNode * pPare
 
 void CCLabelTTFLoader::onHandlePropTypeIntegerLabeled(CCNode * pNode, CCNode * pParent, CCString * pPropertyName, int pIntegerLabeled, CCBReader * pCCBReader) {
     if(pPropertyName->compare(PROPERTY_HORIZONTALALIGNMENT) == 0) {
-        ((CCLabelTTF *)pNode)->setHorizontalAlignment(CCTextAlignment(pIntegerLabeled));
+        ((CCLabelTTF *)pNode)->setHorizontalAlignment(TextHAlignment(pIntegerLabeled));
     } else if(pPropertyName->compare(PROPERTY_VERTICALALIGNMENT) == 0) {
-        ((CCLabelTTF *)pNode)->setVerticalAlignment(CCVerticalTextAlignment(pIntegerLabeled));
+        ((CCLabelTTF *)pNode)->setVerticalAlignment(TextHAlignment(pIntegerLabeled));
     } else {
         CCNodeLoader::onHandlePropTypeFloatScale(pNode, pParent, pPropertyName, pIntegerLabeled, pCCBReader);
     }