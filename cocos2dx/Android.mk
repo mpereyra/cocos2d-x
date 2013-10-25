f6536be77a020bd5ea9c039c4e1f2a13f8badfc7
fuer <fu-er@brooklynpacket.com>
2013-10-25 11:37:34 -0700

    Get Android to compile with FG

--- cocos2dx/Android.mk
+++ cocos2dx/Android.mk
@@ -163,6 +163,7 @@ LOCAL_EXPORT_CFLAGS := -DUSE_FILE32API
 
 include $(BUILD_STATIC_LIBRARY)
 
+$(call import-add-path,$(LOCAL_PATH)/platform/third_party/android/prebuilt)
 $(call import-module,libjpeg)
 $(call import-module,libpng)
 $(call import-module,libxml2)
