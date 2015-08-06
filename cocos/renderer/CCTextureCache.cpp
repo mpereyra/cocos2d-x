4d219ad06cbdfb7f70ccd69fe12c9b3e7e241836
Sean Maher <sean@brooklynpacket.com>
2015-08-05 20:10:09 -0700

    fix truncated log in getCachedTextureInfo

--- cocos/renderer/CCTextureCache.cpp
+++ cocos/renderer/CCTextureCache.cpp
@@ -621,7 +621,10 @@ std::string TextureCache::getCachedTextureInfo() const
                (long)bpp,
                (long)bytes / 1024);
         
-        buffer += buftmp;
+        // BPC_PATCH
+        // buffer was not big enough
+        // buffer += buftmp;
+        CCLOG("%s", buftmp);
     }
 
     snprintf(buftmp, sizeof(buftmp)-1, "TextureCache dumpDebugInfo: %ld textures, for %lu KB (%.2f MB)\n", (long)count, (long)totalBytes / 1024, totalBytes / (1024.0f*1024.0f));
