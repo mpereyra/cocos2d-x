9cfdfe90aa5d314021bf066faae4025b571f5a84
jayme <jdedona@gmail.com>
2015-12-10 16:07:07 -0800

    Patch cocos to call onPause on GLSurfaceView parent.

--- cocos/platform/android/java/src/org/cocos2dx/lib/Cocos2dxGLSurfaceView.java
+++ cocos/platform/android/java/src/org/cocos2dx/lib/Cocos2dxGLSurfaceView.java
@@ -185,7 +185,7 @@ public class Cocos2dxGLSurfaceView extends GLSurfaceView {
             }
         });
         this.setRenderMode(RENDERMODE_WHEN_DIRTY);
-        //super.onPause();
+        super.onPause();
     }
 
     @Override
