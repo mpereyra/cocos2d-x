67700ba67e297c50fb80d9c67341e0b3b509211a
Sean Maher <sean@brooklynpacket.com>
2016-03-30 09:55:59 -0700

    build server cocos include fix

--- cocos/platform/android/javaactivity-android.cpp
+++ cocos/platform/android/javaactivity-android.cpp
@@ -38,7 +38,7 @@ THE SOFTWARE.
 #include "platform/android/jni/JniHelper.h"
 #include <android/log.h>
 #include <jni.h>
-#include "../../plugin/publish/protocols/android/PluginJniHelper.h"
+#include "../../../plugin/publish/protocols/android/PluginJniHelper.h"
 
 #define  LOG_TAG    "main"
 #define  LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG,LOG_TAG,__VA_ARGS__)
