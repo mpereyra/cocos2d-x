a0b4e63c46dc8c4b8974c63dc9e54d3374730e3f
Sean Maher <sean@brooklynpacket.com>
2017-06-21 14:43:23 -0700

    dont fake FPS in debug mode anymore

--- cocos/base/CCDirector.cpp
+++ cocos/base/CCDirector.cpp
@@ -351,11 +351,14 @@ void Director::calculateDeltaTime()
     }
 
 #if COCOS2D_DEBUG
+    // BPC patched this out; makes frame rate innacurate and
+    // slower devices even slower.
+    
     // If we are debugging our code, prevent big delta time
-    if (_deltaTime > 0.2f)
-    {
-        _deltaTime = 1 / 60.0f;
-    }
+//    if (_deltaTime > 0.2f)
+//    {
+//        _deltaTime = 1 / 60.0f;
+//    }
 #endif
 
     _lastUpdate = now;
