b24b18d45d73ed51cc702cfc836988c02a89e81e
brooke <brooke@tinyco.com>
2018-04-27 15:10:44 -0700

    better fix for device-specific parseInt crash - try/catch

--- cocos/platform/android/java/src/org/cocos2dx/lib/Cocos2dxHelper.java
+++ cocos/platform/android/java/src/org/cocos2dx/lib/Cocos2dxHelper.java
@@ -127,12 +127,11 @@ public class Cocos2dxHelper {
                 AudioManager am = (AudioManager) activity.getSystemService(Context.AUDIO_SERVICE);
                 String strSampleRate = am.getProperty(AudioManager.PROPERTY_OUTPUT_SAMPLE_RATE);
                 String strBufferSizeInFrames = am.getProperty(AudioManager.PROPERTY_OUTPUT_FRAMES_PER_BUFFER);
-
-                if (!strSampleRate.isEmpty()) {
+                try {
                     sampleRate = Integer.parseInt(strSampleRate);
-                }
-                if (!strBufferSizeInFrames.isEmpty()) {
                     bufferSizeInFrames = Integer.parseInt(strBufferSizeInFrames);
+                } catch (NumberFormatException e) {
+                    Log.e(TAG, "parseInt failed", e);
                 }
 
                 Log.d(TAG, "sampleRate: " + sampleRate + ", framesPerBuffer: " + bufferSizeInFrames);
