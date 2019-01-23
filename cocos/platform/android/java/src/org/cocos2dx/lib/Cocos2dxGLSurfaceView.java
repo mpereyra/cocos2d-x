e9946afa2089b0bb47ce53979913cafc8864ed12
Casey Kirkruff <casey@brooklynpacket.com>
2019-01-23 11:54:19 -0800

    Android ANR: Call renderer pause/resume directly instead of adding to render queue

--- cocos/platform/android/java/src/org/cocos2dx/lib/Cocos2dxGLSurfaceView.java
+++ cocos/platform/android/java/src/org/cocos2dx/lib/Cocos2dxGLSurfaceView.java
@@ -183,22 +183,12 @@ public class Cocos2dxGLSurfaceView extends GLSurfaceView {
     public void onResume() {
         super.onResume();
         this.setRenderMode(RENDERMODE_CONTINUOUSLY);
-        this.queueEvent(new Runnable() {
-            @Override
-            public void run() {
-                Cocos2dxGLSurfaceView.this.mCocos2dxRenderer.handleOnResume();
-            }
-        });
+        Cocos2dxGLSurfaceView.this.mCocos2dxRenderer.handleOnResume();
     }
 
     @Override
     public void onPause() {
-        this.queueEvent(new Runnable() {
-            @Override
-            public void run() {
-                Cocos2dxGLSurfaceView.this.mCocos2dxRenderer.handleOnPause();
-            }
-        });
+        Cocos2dxGLSurfaceView.this.mCocos2dxRenderer.handleOnPause();
         this.setRenderMode(RENDERMODE_WHEN_DIRTY);
         super.onPause();
     }
